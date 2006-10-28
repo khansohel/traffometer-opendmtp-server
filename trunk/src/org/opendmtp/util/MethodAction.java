a09170f8565e2a869e58925b448a1d75fc24a0f1
mark.lee.stillwell <mark.lee.stillwell@287ead98-9220-0410-bbe7-83a9f409cdb1>
2006-10-28 08:17:33 +0000

    Added a lot of javadocs to MethodAction and OrderedMap.  These classes seem like they could be done away with, though it would require rewriting the code that uses them.

--- trunk/src/org/opendmtp/util/MethodAction.java
+++ trunk/src/org/opendmtp/util/MethodAction.java
@@ -34,10 +34,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * FIXME: not very good
- * 
- * some sort of reflection api helper lib
- * 
+ * This class provides a way to invoke an object's methods in another thread.
  * @author Martin D. Flynn
  * @author Mark Stillwell
  */
@@ -65,21 +62,48 @@ public class MethodAction implements ActionListener, Runnable {
   // argClass: method arugment types (or null of no arguments)
   // args : method arguments (or null of no arguments)
 
-  public MethodAction(Object targ, String methName) throws NoSuchMethodException,
-      ClassNotFoundException {
+  /**
+   * Creates a new MethodAction object that can be used to run the method of the given name of the 
+   * given object in a new thread.
+   * @param targ an object
+   * @param methName the name of a method of the given object
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the given class does not have a method with the given name
+   */
+  public MethodAction(Object targ, String methName) 
+      throws NoSuchMethodException, ClassNotFoundException {
     this(targ, methName, null, null);
   }
 
-  public MethodAction(Object targ, String methName, Class argClass[]) throws NoSuchMethodException,
-      ClassNotFoundException {
+  /**
+   * Creates a new MethodAction object that can be used to run the method with the given name and
+   * argument types of the given object in a new thread.
+   * @param targ an object
+   * @param methName the name of a method of the given object
+   * @param argClass an array of the argument types of the given method
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the given class does not have a method with the given name
+   */  
+  public MethodAction(Object targ, String methName, Class argClass[])
+      throws NoSuchMethodException, ClassNotFoundException {
     this(targ, methName, argClass, null);
   }
 
+  /**
+   * Creates a new MethodAction object that can be used to run the method with the given name and 
+   * argument types of the given object using arguments of the given values in a new thread.
+   * @param targ an object
+   * @param methName the name of a method of the given object
+   * @param argClass an array of the argument types of the method
+   * @param args an array of argument values to use when invoking the method
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the given class does not have a method with the given name
+   */  
   public MethodAction(Object targ, String methName, Class argClass[], Object args[])
       throws NoSuchMethodException, ClassNotFoundException {
     this.target = (targ instanceof String) ? Class.forName((String) targ) : targ;
-    this.targetClass = (this.target instanceof Class) ? (Class) this.target : this.target
-        .getClass();
+    this.targetClass = 
+        (this.target instanceof Class) ? (Class) this.target : this.target.getClass();
     this.argClass = (argClass != null) ? argClass : new Class[0];
     if (methName == null) {
       this.method = this.targetClass.getConstructor(this.argClass);
@@ -95,28 +119,75 @@ public class MethodAction implements ActionListener, Runnable {
 
   // ------------------------------------------------------------------------
 
+  /**
+   * Returns a MethodAction object that can by used to run the get method for the field with
+   * the given name of the given object.
+   * @param targ an object with a field of the given name
+   * @param fieldName the name of a field
+   * @return a MethodAction object
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the given class does not have a method with the given name
+\   */
   public static MethodAction GetterMethod(Object targ, String fieldName)
       throws NoSuchMethodException, ClassNotFoundException {
     String mn = _beanMethodName("get", fieldName);
     return new MethodAction(targ, mn, null);
   }
 
-  public static String InvokeGetter(Object targ, String fieldName) throws NoSuchMethodException,
-      ClassNotFoundException, Throwable {
+  /**
+   * Returns a string derived from the return value of the get method of the field with the
+   * given name in the given object.
+   * @param targ an object
+   * @param fieldName the name of a field of the given object
+   * @return a string derived from the value of the field of the given object with the given name
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the object does not have an accessor method for the field
+   * @throws Throwable if the invoked accessor method throws something
+   */
+  public static String InvokeGetter(Object targ, String fieldName) 
+      throws NoSuchMethodException, ClassNotFoundException, Throwable {
     return GetterMethod(targ, fieldName).invoke().toString();
   }
 
+  /**
+   * Returns a MethodAction object that can by used to run the set method for the field with
+   * the given name of the given object.
+   * @param targ an object with a field of the given name
+   * @param fieldName the name of a field
+   * @return a MethodAction object
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the given class does not have a method with the given name
+   */
   public static MethodAction SetterMethod(Object targ, String fieldName)
       throws NoSuchMethodException, ClassNotFoundException {
     String mn = _beanMethodName("set", fieldName);
     return new MethodAction(targ, mn, new Class[] { String.class });
   }
 
+  /**
+   * Invokes the set method of the field with the given name in the given object, passing a string
+   * representing the value to set.
+   * @param targ an object
+   * @param fieldName the name of a field of the given object
+   * @param value a string representing the value to set the field with the given name to
+   * @return a string derived from the value of the field of the given object with the given name
+   * @throws ClassNotFoundException if the class of the given object can not be found
+   * @throws NoSuchMethodException if the object does not have an accessor method for the field
+   * @throws Throwable if the invoked accessor method throws something
+   */
   public static void InvokeSetter(Object targ, String fieldName, String value)
       throws NoSuchMethodException, ClassNotFoundException, Throwable {
     SetterMethod(targ, fieldName).invoke(new Object[] { value });
   }
 
+  /**
+   * Returns a string created by capitalizing the first letter of fieldName appending it to prefix.
+   * If prefix is "get" or "set" and fieldName is the name of a private field of a javabean, then 
+   * the return value is the name of a standard javabean accessor method. 
+   * @param prefix the string prefix
+   * @param fieldName the name of the field of a javabean
+   * @return the name of an accessor method for the field with the given name
+   */
   protected static String _beanMethodName(String prefix, String fieldName) {
     StringBuffer sb = new StringBuffer(prefix);
     sb.append(fieldName.substring(0, 1).toUpperCase());
@@ -126,25 +197,47 @@ public class MethodAction implements ActionListener, Runnable {
 
   // ------------------------------------------------------------------------
 
+  /**
+   * Returns the target object of this MethodAction object.
+   * @return the target object
+   */
   public Object getTarget() {
     return this.target;
   }
 
+  /**
+   * Sets the arguments to the method which can be invoked by this MethodAction object.
+   * @param args an array of argument values
+   */
   public void setArgs(Object args[]) {
     this.args = args;
   }
 
+  /**
+   * Gets the current arguments to the method which can be invoked by this MethodAction object.
+   * @return an array of argument values
+   */
   public Object[] getArgs() {
     return this.args;
   }
 
   // ------------------------------------------------------------------------
 
+  /**
+   * Invokes the method of the target object which can be invoked by this MethodAction object.
+   * @param args an array of argument values
+   * @throws Throwable if the invoked method throws something
+   */
   public Object invoke(Object args[]) throws Throwable {
     this.setArgs(args);
     return this.invoke();
   }
 
+  /**
+   * Invokes the method of the target object which can be invoked by this MethodAction object.
+   * @return the return value of the invoked method
+   * @throws Throwable if the invoked method throws something
+   */
   public Object invoke() throws Throwable {
     this.error = null;
     this.rtnValue = null;
@@ -170,12 +263,21 @@ public class MethodAction implements ActionListener, Runnable {
     }
   }
 
+  /**
+   * Returns the last return value of the invoked method of the target object. Returns null if the
+   * method has not been invoked.   
+   * @return the return value of the invoked method
+   */
   public Object getReturnValue() {
     return this.rtnValue;
   }
 
   // ------------------------------------------------------------------------
 
+  /**
+   * Delays for the given number of milliseconds.
+   * @param the number of milliseconds to delay
+   */
   public void invokeDelayed(int delayMillis) {
     javax.swing.Timer delay = new javax.swing.Timer(delayMillis, this);
     delay.setRepeats(false);
@@ -210,6 +312,9 @@ public class MethodAction implements ActionListener, Runnable {
     MethodAction.invokeAndWait(this);
   }
 
+  /**
+   * Invokes method when a Thread is started to run this object.
+   */
   public void run() {
     try {
       this.invoke();
--- trunk/src/org/opendmtp/util/OrderedMap.java
+++ trunk/src/org/opendmtp/util/OrderedMap.java
@@ -25,267 +25,317 @@
 // ----------------------------------------------------------------------------
 package org.opendmtp.util;
 
-import java.io.*;
-import java.util.*;
-import java.net.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
 
-public class OrderedMap
-    extends HashMap
-    implements Map
-{
-    
-    // ------------------------------------------------------------------------
-    
-    private OrderedSet  keyOrder = null;
-    private Map         ignoredCaseMap = null;
-    
-    public OrderedMap() 
-    {
-        super();
-        this.keyOrder = new OrderedSet();
-    }
-    
-    public OrderedMap(Map map) 
-    {
-        this();
-        this.putAll(map);
-    }
-    
-    // ------------------------------------------------------------------------
+/**
+ * This class implements a HashMap using an OrderedSet to hold key values.  
+ * 
+ * @author Martin D. Flynn
+ * @author Mark Stillwell
+ */
+public class OrderedMap extends HashMap implements Map {
 
-    public boolean isIgnoreCase()
-    {
-        return (this.ignoredCaseMap != null);
-    }
-    
-    public void setIgnoreCase(boolean ignoreCase)
-    {
-        if (ignoreCase) {
-            if (this.ignoredCaseMap == null) {
-                this.ignoredCaseMap = new HashMap();
-                for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
-                    Object key = i.next();
-                    if (key instanceof String) {
-                        this.ignoredCaseMap.put(((String)key).toLowerCase(), key);
-                    }
-                }
-            }
-        } else {
-            if (this.ignoredCaseMap != null) {
-                this.ignoredCaseMap = null;
-            }
-        }
-    }
-    
-    public Object keyCaseFilter(Object key)
-    {
-        if ((this.ignoredCaseMap != null) && (key instanceof String)) {
-            Object k = this.ignoredCaseMap.get(((String)key).toLowerCase());
-            if (k != null) {
-                //if (!k.equals(key)) { Print.logStackTrace("Filtered key: " + key + " ==> " + k); }
-                return k;
-            }
-        }
-        return key;
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public void clear() 
-    {
-        super.clear();
-        this.keyOrder.clear();
-        if (this.ignoredCaseMap != null) { 
-            this.ignoredCaseMap.clear(); 
-        }
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public Set entrySet()
-    {
-        // Attempting to return an ordered set of 'Map.Entry' entries.
-        // The effect this will have on calls to this method from HashMap itself
-        // isn't fully known.
+  // ------------------------------------------------------------------------
 
-        /* Map.Entry map */
-        Set es = super.entrySet(); // unordered
-        HashMap meMap = new HashMap();
-        for (Iterator i = es.iterator(); i.hasNext();) {
-            Map.Entry me = (Map.Entry)i.next();
-            Object key = me.getKey();
-            meMap.put(key, me);
-        }
-        
-        /* place in keyOrder */
-        OrderedSet entSet = new OrderedSet();
+  private OrderedSet keyOrder = null;
+  private Map ignoredCaseMap = null;
+
+  public OrderedMap() {
+    super();
+    this.keyOrder = new OrderedSet();
+  }
+
+  public OrderedMap(Map map) {
+    this();
+    this.putAll(map);
+  }
+
+  // ------------------------------------------------------------------------
+
+  public boolean isIgnoreCase() {
+    return (this.ignoredCaseMap != null);
+  }
+
+  public void setIgnoreCase(boolean ignoreCase) {
+    if (ignoreCase) {
+      if (this.ignoredCaseMap == null) {
+        this.ignoredCaseMap = new HashMap();
         for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
-            Object key = i.next();
-            Object me  = meMap.get(key);
-            if (me == null) { Print.logError("Map.Entry is null!!!"); }
-            entSet.add(me);
+          Object key = i.next();
+          if (key instanceof String) {
+            this.ignoredCaseMap.put(((String) key).toLowerCase(), key);
+          }
         }
-        return entSet;
+      }
+    }
+    else {
+      if (this.ignoredCaseMap != null) {
+        this.ignoredCaseMap = null;
+      }
+    }
+  }
 
+  public Object keyCaseFilter(Object key) {
+    if ((this.ignoredCaseMap != null) && (key instanceof String)) {
+      Object k = this.ignoredCaseMap.get(((String) key).toLowerCase());
+      if (k != null) {
+        // if (!k.equals(key)) { Print.logStackTrace("Filtered key: " + key + " ==> " + k); }
+        return k;
+      }
     }
-    
-    // ------------------------------------------------------------------------
+    return key;
+  }
 
-    public Set keySet()
-    {
-        return new OrderedSet(this.keyOrder);
+  // ------------------------------------------------------------------------
+
+  /**
+   * Removes all entries from this map.
+   */
+  public void clear() {
+    super.clear();
+    this.keyOrder.clear();
+    if (this.ignoredCaseMap != null) {
+      this.ignoredCaseMap.clear();
     }
-    
-    public Set valueSet()
-    {
-        OrderedSet valSet = new OrderedSet();
-        for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
-            valSet.add(super.get(i.next()));
-        }
-        return valSet;
+  }
+
+  // ------------------------------------------------------------------------
+
+  public Set entrySet() {
+    // Attempting to return an ordered set of 'Map.Entry' entries.
+    // The effect this will have on calls to this method from HashMap itself
+    // isn't fully known.
+
+    /* Map.Entry map */
+    Set es = super.entrySet(); // unordered
+    HashMap meMap = new HashMap();
+    for (Iterator i = es.iterator(); i.hasNext();) {
+      Map.Entry me = (Map.Entry) i.next();
+      Object key = me.getKey();
+      meMap.put(key, me);
     }
-    
-    // ------------------------------------------------------------------------
-        
-    public Collection values()
-    {
-        // All this work is to make sure the returned Collection is still backed by the Map
-        return new ListTools.CollectionProxy(super.values()) {
-            public Iterator iterator() {
-                return new Iterator() {
-                    private Iterator i = OrderedMap.this.keySet().iterator();
-                    public boolean hasNext() {
-                        return i.hasNext();
-                    }
-                    public Object next() {
-                        return OrderedMap.this.get(i.next());
-                    }
-                    public void remove() {
-                        throw new UnsupportedOperationException("'remove' not supported here");
-                    }
-                };
-            }
-            public Object[] toArray() {
-                return ListTools.toList(this.iterator()).toArray();
-            }
-            public Object[] toArray(Object a[]) {
-                return ListTools.toList(this.iterator()).toArray(a);
-            }
+
+    /* place in keyOrder */
+    OrderedSet entSet = new OrderedSet();
+    for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
+      Object key = i.next();
+      Object me = meMap.get(key);
+      if (me == null) {
+        Print.logError("Map.Entry is null!!!");
+      }
+      entSet.add(me);
+    }
+    return entSet;
+
+  }
+
+  // ------------------------------------------------------------------------
+
+  /**
+   * Returns a Set containing the keys of this map.
+   * @return a Set containing the keys of this map
+   */
+  public Set keySet() {
+    return new OrderedSet(this.keyOrder);
+  }
+
+  /**
+   * Returns a Set containing the values of this map.
+   * @return a Set containing the values of this map
+   */
+  public Set valueSet() {
+    OrderedSet valSet = new OrderedSet();
+    for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
+      valSet.add(super.get(i.next()));
+    }
+    return valSet;
+  }
+
+  // ------------------------------------------------------------------------
+
+  /**
+   * Returns a Collection containing the values of this map.
+   * @return a Collection containing the values of this map.
+   */
+  public Collection values() {
+    // All this work is to make sure the returned Collection is still backed by the Map
+    return new ListTools.CollectionProxy(super.values()) {
+      public Iterator iterator() {
+        return new Iterator() {
+          private Iterator i = OrderedMap.this.keySet().iterator();
+
+          public boolean hasNext() {
+            return i.hasNext();
+          }
+
+          public Object next() {
+            return OrderedMap.this.get(i.next());
+          }
+
+          public void remove() {
+            throw new UnsupportedOperationException("'remove' not supported here");
+          }
         };
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public Object put(int ndx, Object key, Object value) 
-    {
-        if ((this.ignoredCaseMap != null) && (key instanceof String)) {
-            this.ignoredCaseMap.put(((String)key).toLowerCase(), key);
-        }
-        this.keyOrder.add(ndx, key);
-        return super.put(key, value);
-    }
-    
-    public Object put(Object key, Object value) 
-    {
-        if ((this.ignoredCaseMap != null) && (key instanceof String)) {
-            this.ignoredCaseMap.put(((String)key).toLowerCase(), key);
-        }
-        this.keyOrder.add(key);
-        return super.put(key, value);
-    }
-    
-    public Object setProperty(String key, String value) 
-    {
-        return this.put(key, value);
-    }
-    
-    public void putAll(Map map)
-    {
-        for (Iterator i = map.keySet().iterator(); i.hasNext();) {
-            Object key = i.next();
-            this.put(key, map.get(key));
-        }
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public boolean containsKeyIgnoreCase(String key)
-    {
-        if (key != null) {
-            // TODO: Optimize!
-            for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
-                Object k = i.next();
-                if ((k instanceof String) && key.equalsIgnoreCase((String)k)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+      }
 
-    public boolean containsKey(Object key)
-    {
-        return super.containsKey(this.keyCaseFilter(key));
-    }
-    
-    // ------------------------------------------------------------------------
-    
-    public Object remove(Object key)
-    {
-        Object k = this.keyCaseFilter(key);
-        if ((this.ignoredCaseMap != null) && (key instanceof String)) {
-            this.ignoredCaseMap.remove(((String)key).toLowerCase());
-        }
-        this.keyOrder.remove(k);
-        return super.remove(k);
-    }
-        
-    // ------------------------------------------------------------------------
-    
-    public Object get(Object key)
-    {
-        return super.get(this.keyCaseFilter(key));
-    }
-    
-    public String getProperty(String key, String dft)
-    {
-        if (this.containsKey(key)) {
-            Object val = this.get(key);
-            return (val != null)? val.toString() : null;
-        } else {
-            return dft;
-        }
-    }
-    
-    public String getProperty(String key)
-    {
-        return this.getProperty(key, null);
-    }
-    
-    // ------------------------------------------------------------------------
+      public Object[] toArray() {
+        return ListTools.toList(this.iterator()).toArray();
+      }
 
-    public Object getKey(int ndx)
-    {
-        return ((ndx >= 0) && (ndx < this.keyOrder.size()))? this.keyOrder.get(ndx) : null;
-    }
+      public Object[] toArray(Object a[]) {
+        return ListTools.toList(this.iterator()).toArray(a);
+      }
+    };
+  }
 
-    public Object getValue(int ndx)
-    {
-        Object key = this.getKey(ndx);
-        return (key != null)? super.get(key) : null;
+  // ------------------------------------------------------------------------
+
+  /**
+   * Puts a value in this Map with the given key in position ndx.
+   * @param position to put the key in this map
+   * @param key to map to value
+   * @param value to be put in this Map
+   */
+  public Object put(int ndx, Object key, Object value) {
+    if ((this.ignoredCaseMap != null) && (key instanceof String)) {
+      this.ignoredCaseMap.put(((String) key).toLowerCase(), key);
     }
-    
-    public void remove(int ndx)
-    {
-        this.remove(this.getKey(ndx));
+    this.keyOrder.add(ndx, key);
+    return super.put(key, value);
+  }
+
+  /**
+   * Puts a value in this Map with the given key.  The key is placed in the last position.
+   * @param key to map to value
+   * @param value to be put in this Map
+   */
+  public Object put(Object key, Object value) {
+    if ((this.ignoredCaseMap != null) && (key instanceof String)) {
+      this.ignoredCaseMap.put(((String) key).toLowerCase(), key);
     }
-    
-    public Iterator keys() 
-    {
-        // copied to prevent 'remove'
-        return (new Vector(this.keyOrder)).iterator(); 
+    this.keyOrder.add(key);
+    return super.put(key, value);
+  }
+
+  public Object setProperty(String key, String value) {
+    return this.put(key, value);
+  }
+
+  /**
+   * Adds the contents of the given map to the end of this map.
+   * @param a map to add to the end of this one
+   */
+  public void putAll(Map map) {
+    for (Iterator i = map.keySet().iterator(); i.hasNext();) {
+      Object key = i.next();
+      this.put(key, map.get(key));
     }
-    
-    // ------------------------------------------------------------------------
-    
+  }
+
+  // ------------------------------------------------------------------------
+
+  /**
+   * Tests if this map has the given key without considering case.
+   * @return true if this map has the given key (ignores case)
+   */
+  public boolean containsKeyIgnoreCase(String key) {
+    if (key != null) {
+      // TODO: Optimize!
+      for (Iterator i = this.keyOrder.iterator(); i.hasNext();) {
+        Object k = i.next();
+        if ((k instanceof String) && key.equalsIgnoreCase((String) k)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Tests if this map has the given key.  This method does not ignore case.
+   * @return true if this map has the given key
+   */
+  public boolean containsKey(Object key) {
+    return super.containsKey(this.keyCaseFilter(key));
+  }
+
+  // ------------------------------------------------------------------------
+
+  public Object remove(Object key) {
+    Object k = this.keyCaseFilter(key);
+    if ((this.ignoredCaseMap != null) && (key instanceof String)) {
+      this.ignoredCaseMap.remove(((String) key).toLowerCase());
+    }
+    this.keyOrder.remove(k);
+    return super.remove(k);
+  }
+
+  // ------------------------------------------------------------------------
+
+  public Object get(Object key) {
+    return super.get(this.keyCaseFilter(key));
+  }
+
+  public String getProperty(String key, String dft) {
+    if (this.containsKey(key)) {
+      Object val = this.get(key);
+      return (val != null) ? val.toString() : null;
+    }
+    else {
+      return dft;
+    }
+  }
+
+  public String getProperty(String key) {
+    return this.getProperty(key, null);
+  }
+
+  // ------------------------------------------------------------------------
+
+  /**
+   * Returns the key in position ndx. Returns null if ndx is less than 0 or larger than the
+   * position of the last key.
+   * @param the index of the key to return
+   * @return the key at position ndx 
+   */
+  public Object getKey(int ndx) {
+    return ((ndx >= 0) && (ndx < this.keyOrder.size())) ? this.keyOrder.get(ndx) : null;
+  }
+
+  /**
+   * Returns the value in position ndx. Returns null if ndx is less than 0 or larger than the
+   * position of the last value.
+   * @param the index of the key to return
+   * @return the value at position ndx
+   */
+  public Object getValue(int ndx) {
+    Object key = this.getKey(ndx);
+    return (key != null) ? super.get(key) : null;
+  }
+
+  /**
+   * Removes the key/value pair at position ndx.
+   * @param the index of the key/value pair to remove
+   */
+  public void remove(int ndx) {
+    this.remove(this.getKey(ndx));
+  }
+
+  /**
+   * Returns an Iterator over the keys for this map. The Iterator returns keys in the order
+   * maintained by this Map.
+   * @return an Iterator over the keys for this map
+   */
+  public Iterator keys() {
+    // copied to prevent 'remove'
+    return (new Vector(this.keyOrder)).iterator();
+  }
+
+  // ------------------------------------------------------------------------
+
 }
