3074e9e9a93a3522161cc25992950cf0ab510cfb
brandonhawaii <brandonhawaii@287ead98-9220-0410-bbe7-83a9f409cdb1>
2006-10-31 17:55:11 +0000

    javadoc

--- trunk/src/org/opendmtp/server/base/AccountID.java
+++ trunk/src/org/opendmtp/server/base/AccountID.java
@@ -22,97 +22,124 @@
 // ----------------------------------------------------------------------------
 package org.opendmtp.server.base;
 
-import java.lang.*;
-import java.util.*;
-import java.io.*;
-import java.net.*;
-import java.sql.*;
+import org.opendmtp.codes.ServerErrors;
+import org.opendmtp.server.db.AccountDB;
+import org.opendmtp.util.Print;
 
-import org.opendmtp.util.*;
-import org.opendmtp.codes.*;
-import org.opendmtp.server.db.*;
 
-public class AccountID
-{
 
-    // ------------------------------------------------------------------------
-    // - AccountID
-    //      - AccountID name [key]
-    //      - Owner name
-    //      - Contact email
-    //      - Event notification email
-    //      - Account level of service
+/**
+ * AccountID, seems to provide access to existing AccountDB information to check if it is
+ * active, name, and a toString method.  Uses a private variable but and a private static 
+ * constructor, but public accessors and a public static method to "load account".
+ * 
+ * @author Martin D. Flynn
+ * @author Brandon Lee
+ */
+public class AccountID {
 
-    /* identification */
-    private AccountDB db = null;
- 
-    // ------------------------------------------------------------------------
+  // - AccountID
+  // - AccountID name [key]
+  // - Owner name
+  // - Contact email
+  // - Event notification email
+  // - Account level of service
 
-    private static AccountDB GetAccountDB(String acctName)
-        throws PacketParseException
-    {
-        DMTPServer.DBFactory fact = DMTPServer.getDBFactory();
-        if (fact != null) {
-            AccountDB db = fact.getAccountDB(acctName);
-            if (db != null) {
-                return db;
-            }
-        } else {
-            Print.logError("No factory registered for AccountID");
-        }
-        return null;
+  /**
+   * Identification, used for varification.
+   */
+  private AccountDB db = null;
+
+  /**
+   * Returns a AccountDB from the DMTPServer.DBFactory.  
+   * If not found returns null.
+   * 
+   * @param acctName containing the accountID.
+   * @return AccountDB contains the account specified or null.
+   * @throws PacketParseException not sure why.
+   */
+  private static AccountDB GetAccountDB(String acctName) throws PacketParseException {
+    
+    DMTPServer.DBFactory fact = DMTPServer.getDBFactory();
+    
+    if (fact != null) {
+      AccountDB db = fact.getAccountDB(acctName);
+      if (db != null) {
+        return db;
+      }
     }
-
-    // ------------------------------------------------------------------------
-
-    public static AccountID loadAccountID(String acctName)
-        throws PacketParseException
-    {
-        return new AccountID(acctName);
+    else {
+      Print.logError("No factory registered for AccountID");
     }
+    return null;
+  }
 
-    // ------------------------------------------------------------------------
+  /**
+   * Calls the private constructor to create a new public AccountID instance with param 
+   * acctName then returns that instance.
+   * 
+   * @param acctName a string containing the DBAccount name or ID.
+   * @return The instance of the newly created AccountID  the new.
+   * @throws PacketParseException if name equals null or "".
+   */
+  public static AccountID loadAccountID(String acctName) throws PacketParseException {
+    
+    return new AccountID(acctName);
+  }
 
-    private AccountID(String acctName)
-        throws PacketParseException
-    {
-        
-        /* null? */
-        if ((acctName == null) || acctName.equals("")) {
-            Print.logError("Account name is null/empty");
-            throw new PacketParseException(ServerErrors.NAK_ACCOUNT_INVALID, null); // errData ok
-        }
-        
-        /* device exists? */
-        this.db = GetAccountDB(acctName);
-        if (this.db == null) {
-            Print.logError("AccountID not found: " + acctName);
-            throw new PacketParseException(ServerErrors.NAK_ACCOUNT_INVALID, null); // errData ok
-        }
-        
-    }
+  /**
+   * Private constructor that attaches an DBAcount to the private variable db.  db can result
+   * in either null, or the desired AccountID else.  Throws a PacketParseException if the result
+   * is null, string acctName is null or empty.
+   * 
+   * @param acctName the name of the new AccountID
+   * @throws PacketParseException throws exception if acctName or bad or not found.
+   */
+  private AccountID(String acctName) throws PacketParseException {
 
-    // ------------------------------------------------------------------------
-
-    public String getAccountName()
-    {
-        return this.db.getAccountName();
+    // throws exception if acctName is null or ""
+    if ((acctName == null) || acctName.equals("")) {
+      
+      Print.logError("Account name is null/empty");
+      throw new PacketParseException(ServerErrors.NAK_ACCOUNT_INVALID, null); // errData ok
     }
     
-    // ------------------------------------------------------------------------
+    // continues if name is ok, throws exception if account doesn't exsist else set to db.
+    this.db = GetAccountDB(acctName);
+    if (this.db == null) {
+      
+      Print.logError("AccountID not found: " + acctName);
+      throw new PacketParseException(ServerErrors.NAK_ACCOUNT_INVALID, null); // errData ok
+    } //end if
+  }
 
-    public boolean isActive()
-    {
-        return this.db.isActive();
-    }
+  /**
+   * Returns a String with the accounts name.
+   * 
+   * @return String containing the account's name.
+   */
+  public String getAccountName() {
     
-    // ------------------------------------------------------------------------
+    return this.db.getAccountName();
+  }
 
-    public String toString()
-    {
-        return (this.db != null)? this.db.toString() : "";
-    }
-
-    // ------------------------------------------------------------------------
+  /**
+   * Checks to see if the database is active.
+   * 
+   * @return boolean true if active false otherwise.
+   */
+  public boolean isActive() {
     
+    return this.db.isActive();
+  }
+
+  /**
+   * Overwrites the toString method and returns db as a string or null.
+   * 
+   * @return String the db as a string.
+   */
+  public String toString() {
+    
+    return (this.db != null) ? this.db.toString() : "";
+  }
 }
