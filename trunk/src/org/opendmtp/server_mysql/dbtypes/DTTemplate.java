be4e9dae6da92f50d9873755cfc4a2da99c5d7f3
rbrewer <rbrewer@287ead98-9220-0410-bbe7-83a9f409cdb1>
2006-11-02 10:23:25 +0000

    More updated javadocs, and a package.html thrown in for fun.

--- trunk/src/org/opendmtp/server_mysql/dbtypes/DTTemplate.java
+++ trunk/src/org/opendmtp/server_mysql/dbtypes/DTTemplate.java
@@ -29,6 +29,12 @@ import org.opendmtp.dbtools.DBFieldType;
 import org.opendmtp.util.RTProperties;
 import org.opendmtp.util.StringTools;
 
+/**
+ * Some sort of wrapper for property templates.
+ * 
+ * @author Martin D. Flynn
+ * @author Robert S. Brewer
+ */
 public class DTTemplate extends DBFieldType {
 
   // ------------------------------------------------------------------------
@@ -42,22 +48,42 @@ public class DTTemplate extends DBFieldType {
 
   private RTProperties templateProps = null;
 
+  /**
+   * Initializes template properties field.
+   */
   public DTTemplate() {
     super();
     this.templateProps = new RTProperties("");
   }
 
+  /**
+   * Initializes template properties field using provided template.
+   * 
+   * @param template string containing properties template. 
+   */
   public DTTemplate(String template) {
     super(template);
     this.templateProps = new RTProperties((template != null) ? template : "");
   }
 
+  /**
+   * Initializes template properties field using result set from SQL database.
+   * 
+   * @param rs result set from SQL database query.
+   * @param fldName name of field in database.
+   * @throws SQLException if there is an error while accessing the database.
+   */
   public DTTemplate(ResultSet rs, String fldName) throws SQLException {
     super(rs, fldName);
     // set to default value if 'rs' is null
     this.templateProps = new RTProperties((rs != null) ? rs.getString(fldName) : "");
   }
 
+  /**
+   * Provides textual representation of field.
+   * 
+   * @return String containing textual representation.
+   */
   public String toString() {
     return this.templateProps.toString();
   }
@@ -67,27 +93,56 @@ public class DTTemplate extends DBFieldType {
 
   private static final char FIELD_VALUE_SEPARATOR = '|';
 
+  /**
+   * Clears the properties in the class field.
+   */
   public void clearFields() {
     this.templateProps.clearProperties();
   }
 
+  /**
+   * Returns a field extracted from the template properties at the requested index.
+   * 
+   * @param ndx index of field to be extracted from properties.
+   * @return extracted field.
+   */
   public Field getField(int ndx) {
     String name = String.valueOf(ndx);
     String ftmp = this.templateProps.getString(name, null);
     return ((ftmp != null) && !ftmp.equals("")) ? new Field(ftmp) : null;
   }
 
+  /**
+   * Sets a field in the template properties at the requested index.
+   * 
+   * @param ndx index of field to be set in properties.
+   * @param fld field value to be set in properties.
+   */
   public void setField(int ndx, Field fld) {
     String name = String.valueOf(ndx);
     this.templateProps.setString(name, fld.toString());
   }
 
+  /**
+   * Wrapper for database fields of this type.
+   * 
+   * @author Martin D. Flynn
+   * @author Robert S. Brewer
+   */
   public static class Field {
     private int type = -1;
     private boolean isHiRes = false;
     private int index = 0;
     private int length = 0;
 
+    /**
+     * Initializes class fields values using given values.
+     * 
+     * @param type type of field.
+     * @param hiRes true if high resolution, false otherwise.
+     * @param ndx index in properties.
+     * @param len length of field.
+     */
     public Field(int type, boolean hiRes, int ndx, int len) {
       this.type = type;
       this.isHiRes = hiRes;
@@ -95,6 +150,11 @@ public class DTTemplate extends DBFieldType {
       this.length = len;
     }
 
+    /**
+     * Initializes class fields values using values parsed out of given String.
+     * 
+     * @param s String to be parsed for values.
+     */
     public Field(String s) {
       String f[] = StringTools.parseString(s, FIELD_VALUE_SEPARATOR);
       this.type = (f.length > 0) ? StringTools.parseInt(f[1], -1) : -1;
@@ -103,22 +163,49 @@ public class DTTemplate extends DBFieldType {
       this.length = (f.length > 3) ? StringTools.parseInt(f[3], 0) : 0;
     }
 
+    /**
+     * Accessor for hiRes field.
+     * 
+     * @return true if field is high resolution, false otherwise.
+     */
     public boolean isHiRes() {
       return this.isHiRes;
     }
 
+    /**
+     * Accessor for type field.
+     * 
+     * @return type of this field.
+     */
     public int getType() {
       return this.type;
     }
 
+    /**
+     * Accessor for index field.
+     * 
+     * @return index of this field.
+     */
     public int getIndex() {
       return this.index;
     }
 
+    /**
+     * Accessor for length field.
+     * 
+     * @return length of this field.
+     */
     public int getLength() {
       return this.length;
     }
 
+    /**
+     * Provides textual representation of field. It will look like:
+     * "#=type|[H|L]|index|length"
+     * 
+     * @return String containing textual representation.
+     * @see java.lang.Object#toString()
+     */
     public String toString() {
       StringBuffer sb = new StringBuffer();
       sb.append(this.isHiRes() ? "H" : "L");
@@ -131,6 +218,12 @@ public class DTTemplate extends DBFieldType {
       return sb.toString();
     }
 
+    /**
+     * Compares two DTTemplates by converting to Strings and then comparing Strings.
+     * 
+     * @param other the other DTTemplate to be compared to.
+     * @return true if the fields of both objects are the same.
+     */
     public boolean equals(Object other) {
       if (other instanceof Field) {
         return this.toString().equals(other.toString());
--- trunk/src/org/opendmtp/server_mysql/dbtypes/DTUniqueID.java
+++ trunk/src/org/opendmtp/server_mysql/dbtypes/DTUniqueID.java
@@ -28,29 +28,56 @@ import java.sql.SQLException;
 import org.opendmtp.dbtools.DBFieldType;
 import org.opendmtp.util.StringTools;
 
+/**
+ * Wrapper for unique ID type.
+ * 
+ * @author Martin D. Flynn
+ * @author Robert S. Brewer
+ */
 public class DTUniqueID extends DBFieldType {
 
   // ------------------------------------------------------------------------
 
   private long uniqueID = 0L;
 
+  /**
+   * Initializes unique ID with provided value.
+   * 
+   * @param uniqueID provided unique ID.
+   */
   public DTUniqueID(long uniqueID) {
     this.uniqueID = uniqueID;
   }
 
+  /**
+   * Initializes unique ID with value provided in String.
+   * 
+   * @param uniqueIDHex String containing unique ID. If 'uniqueIDHex' represents a
+   * 'HEX' value, then it must begin with '0x'. Otherwise it will be parsed as a 'Long'.
+   */
   public DTUniqueID(String uniqueIDHex) {
     super(uniqueIDHex);
-    // If 'uniqueIDHex' represents a 'HEX' value, then it must begin with '0x'.
-    // Otherwise it will be parsed as a 'Long'
     this.uniqueID = StringTools.parseLong(uniqueIDHex, 0L);
   }
 
+  /**
+   * Initializes unique ID using result set from SQL database.
+   * 
+   * @param rs result set from SQL database query.
+   * @param fldName name of field in database.
+   * @throws SQLException if there is an error while accessing the database.
+   */
   public DTUniqueID(ResultSet rs, String fldName) throws SQLException {
     super(rs, fldName);
     // set to default value if 'rs' is null
     this.uniqueID = (rs != null) ? rs.getLong(fldName) : 0L;
   }
 
+  /**
+   * Provides textual representation of field.
+   * 
+   * @return String containing textual representation.
+   */
   public String toString() {
     return String.valueOf(this.uniqueID);
   }
